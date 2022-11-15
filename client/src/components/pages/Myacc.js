f7b164abc138a35f0bc98b03c3a7c28dc1ddcbc0
YuzheLiu2002 <2000012956@stu.pku.edu.cn>
2022-11-15 21:29:29 +0800

    用户界面初步

--- client/src/components/pages/Myacc.js
+++ client/src/components/pages/Myacc.js
@@ -1,12 +1,55 @@
-import React, { useState, useEffect } from "react";
-import "./Myacc.css";
+import "./Myacc.css"
+import React, {useEffect, useState} from "react";
+import {Link, Router} from "@reach/router";
+import "./Mainpage.css"
+import {get} from "../../utilities";
+import TeamPost from "../modules/TeamPost";
 
-const Myacc = () => {
+const TeamUp = () => {
+    const [TeamPosts, setTeamPosts] = useState([]);
+
+    useEffect(() => {
+        document.title = "News TeamPost";
+        get("api/TeamPosts").then((TeamPostObjs) => {
+            let reverseTeamPostObjs = TeamPostObjs.reverse();
+            setTeamPosts(reverseTeamPostObjs);
+        });
+    }, [])
+
+    let TeamPostsList = null;
+    const hasTeamPosts = (TeamPosts.length !== 0);
+
+    if (hasTeamPosts) {
+        TeamPostsList = TeamPosts.map((TeamPostObj) => (
+            <TeamPost
+                key={`TeamPost_${TeamPostObj._id}`}
+                _id={TeamPostObj._id}
+                creator_name={TeamPostObj.creator_name}
+                members_num={TeamPostObj.members_num}
+                team_name={TeamPostObj.team_name}
+                course_name={TeamPostObj.course_name}
+                personal_profile={TeamPostObj.personal_profile}
+                content={TeamPostObj.content}
+            />
+        ));
+    } else {
+        TeamPostsList = <div>No TeamPost!</div>
+    }
+
     return (
-      <div className="Myacc-container">
-        <div className="Myacc-status"/>
-      </div>
-    );
-  };
-  
-  export default Myacc;
+        <div className={"Mainpage-container"}>
+            <div className={"TeamUp-container"}>
+                <div className="MyTeam">管理我的组队
+                </div><br/><br/><br/>
+                <div className={"TeamPost-container"}>
+                {TeamPostsList}
+                <div className={"BottomLine"}>我是有底线的</div>
+                </div>
+            </div>
+        </div>
+
+        //调用展示数据库中已有组队信息接口
+    )
+}
+
+export default TeamUp;
